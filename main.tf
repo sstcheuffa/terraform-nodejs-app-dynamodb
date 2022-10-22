e14bf07254aeac43e4c4798944bedab57ab13c73
sstcheuffa <stcheuffa@alumni.harvard.edu>
2022-10-22 01:37:25 -0400

    created dynamodb.tf and excluded state file in .gitignore

--- main.tf
+++ main.tf
@@ -10,14 +10,3 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_dynamodb_table" "tf_notes_table" {
-  name           = "tf-notes-table"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = "30"
-  write_capacity = "30"
-  attribute {
-    name = "noteId"
-    type = "S"
-  }
-  hash_key = "noteId"
-}
