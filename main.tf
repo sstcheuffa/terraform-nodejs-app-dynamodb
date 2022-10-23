c083b542b85b3168f3924be0b4cb2dfc14fce546
sstcheuffa <stcheuffa@alumni.harvard.edu>
2022-10-23 09:19:04 -0400

    Added note-delete function, added *.zip to .gitignore

--- main.tf
+++ main.tf
@@ -31,3 +31,25 @@ resource "aws_lambda_function" "note-create" {
   role          = aws_iam_role.iam_for_lambda.arn
   filename      = "lambdas/note-create.zip"
 }
+
+data "archive_file" "note-delete-archive" {
+  source_file = "lambdas/note-delete.js"
+  output_path = "lambdas/note-delete.zip"
+  type        = "zip"
+}
+
+resource "aws_lambda_function" "note-delete" {
+  environment {
+    variables = {
+      NOTES_TABLE = aws_dynamodb_table.tf_notes_table.name
+    }
+  }
+  memory_size   = "128"
+  timeout       = 10
+  runtime       = "nodejs14.x"
+  architectures = ["arm64"]
+  handler       = "lambdas/note-delete.handler"
+  function_name = "note-delete"
+  role          = aws_iam_role.iam_for_lambda.arn
+  filename      = "lambdas/note-delete.zip"
+}
