cc42b3840815b560278de69714a0f420daca249f
sstcheuffa <stcheuffa@alumni.harvard.edu>
2022-10-24 00:01:35 -0400

    modified state-management.tf

--- state-management.tf
+++ state-management.tf
@@ -1,16 +1,16 @@
 resource "aws_s3_bucket" "bucket" {
-  bucket = "jenkins-git-terraform-dynamodb-lambda-state-backend"
-  object_lock_enabled = "Enabled"
+  bucket              = "jenkins-git-terraform-dynamodb-lambda-state-backend"
+  object_lock_enabled = "true"
 }
-  
+
 resource "aws_dynamodb_table" "terraform-lock" {
- name = "terraform_state"
- read_capacity = 5
- write_capacity = 5
- hash_key = "LockID"
- attribute {
-   name = "LockID"
-   type = "S"
- }
+  name           = "terraform_state"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 }
 
