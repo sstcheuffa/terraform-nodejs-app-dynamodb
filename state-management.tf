a761ab1a0338bdeba19b5e430b9ec773718b76af
sstcheuffa <stcheuffa@alumni.harvard.edu>
2022-10-24 00:35:57 -0400

    Added versionong to the bucket - state-management.tf line 8

--- state-management.tf
+++ state-management.tf
@@ -1,6 +1,15 @@
 resource "aws_s3_bucket" "bucket" {
   bucket              = "jenkins-git-terraform-dynamodb-lambda-state-backend"
+ 
   object_lock_enabled = "true"
+}
+
+resource "aws_s3_bucket_versioning" "bucket_versioning" {
+    bucket              = "jenkins-git-terraform-dynamodb-lambda-state-backend"
+    versioning_configuration {
+        status = "Enabled"
+        mfa_delete = "Disabled"
+  }
 }
 
 resource "aws_dynamodb_table" "terraform-lock" {
