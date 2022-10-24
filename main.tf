5e88689e2c25f9cf28562c128e07e17695db4ebf
sstcheuffa <stcheuffa@alumni.harvard.edu>
2022-10-23 23:44:17 -0400

    Fixed main.tf, cannot use a variable in the backend block, region

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "pipeline-jenkins-git-terraform-dynamodb-lambda-state-backend"
     key            = "terraform.tfstate"
-    region         = var.aws_region
+    region         = "us-east-1"
     dynamodb_table = "terraform_state"
   }
 
