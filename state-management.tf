ac7ecd1e32b3dfa56f35c0a187f8d0199fa981b9
sstcheuffa <stcheuffa@alumni.harvard.edu>
2022-10-23 23:58:34 -0400

    Create the S3 bucket first before adding the backend

--- state-management.tf
+++ state-management.tf
@@ -1,15 +1,16 @@
-resource "aws_s3_bucket_object_lock_configuration" "bucket" {
+resource "aws_s3_bucket" "bucket" {
   bucket = "jenkins-git-terraform-dynamodb-lambda-state-backend"
   object_lock_enabled = "Enabled"
 }
-
+  
 resource "aws_dynamodb_table" "terraform-lock" {
-  name           = "terraform_state"
-  read_capacity  = 5
-  write_capacity = 5
-  hash_key       = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
+ name = "terraform_state"
+ read_capacity = 5
+ write_capacity = 5
+ hash_key = "LockID"
+ attribute {
+   name = "LockID"
+   type = "S"
+ }
 }
+
